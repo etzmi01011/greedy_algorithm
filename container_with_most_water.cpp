71ab5439af3023f1018900e397cbd2086efd0e71
etzmi01011 <etzmi01011.gmail.com>
2025-02-04 23:06:38 +0800

    first upload

---
+++ container_with_most_water.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    vector<int> height;
+    int ans=0;
+    while(cin>>n){
+        height.push_back(n);
+    }
+    int l=0, r=height.size()-1;
+    while(l<r){
+        int now=min(height[l], height[r])*(r-l);
+        ans=max(ans, now);
+        if(height[l]>height[r]){
+            r--;
+        }
+        else{
+            l++;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
